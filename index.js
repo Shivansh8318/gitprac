c56334d9aa9602f8c9fa1e13482ff7dac5d3e782
Shivansh Agrawal <shivanshagrawal831@gmail.com>
2025-07-02 01:35:31 +0530

    duv

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ function add(a,b){
 function multiply(a,b){
     return a * b;
 }
+function divide(a,b){
+    if (b === 0) {
+        throw new Error("Cannot divide by zero");
+    }
+    return a / b;
+}
